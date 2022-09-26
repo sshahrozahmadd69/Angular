3eddfba94e4ba3af179f0fa6cbfffbb1fedb8dfb
GSK <Sarwar.Khan@O3interfaces.com>
2022-09-26 16:22:39 +0500

    CSS Update

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero[]=[
+    {id:12, name:'Dr Shahroz'},
+    {id:14, name:'Dr Faran'},
+    {id:16, name:'Dr Shery'},
+    {id:18, name:'Dr Sarwar'},
+    {id:20, name:'Dr Ali'},
+    {id:22, name:'Dr Noman'},
+]

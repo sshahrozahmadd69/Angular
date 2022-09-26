8bd365c240738c7326198a6b66c8c7c0ab61ff36
GSK <Sarwar.Khan@O3interfaces.com>
2022-09-26 18:00:13 +0500

    6pm

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,11 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes =HEROES;
+  selectedHero?:Hero;
+  onSelect(hero:Hero):void {
+    this.selectedHero = hero;
+  }
 
- hero: Hero={
-  id: 1,
-  name: 'Shahroz'
- }
  
   constructor() { }
 

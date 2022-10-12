950257da69659ac31e6c5c9f2fdfb0bc3800c0df
GSK <Sarwar.Khan@O3interfaces.com>
2022-10-12 18:19:17 +0500

    navigation first part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,12 +15,22 @@ export class HeroesComponent implements OnInit {
   
 
  
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService ) { }
 
   ngOnInit(): void {
+    this.getHeroes();   
   }
-  onSelect(hero:Hero):void {
+
+  getHeroes():void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+
+  onSelect(hero: Hero): void {
+    this.messageService.add(`You selected Hero with id of ${hero.id} and name ${hero.name} `)
     this.selectedHero = hero;
+
   }
 
   
